76c8493596a586e43c1c68ca25d374854c002afc
galets <git5511@mx1.galets.net>
2015-06-16 21:45:15 -0700

    updated print screen script to display notification

--- sample_print_screen.sh
+++ sample_print_screen.sh
@@ -4,16 +4,17 @@ echo "" | xclip -in -selection clipboard
 
 rm /tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.png
 gnome-screenshot --area --file=/tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.png && (
-    #URL=`/home/galets/Projects/2015/VVCap/VVCapPost/bin/Release/VVCapPost.exe /tmp/ssvvcap.png`
-    URL=`/home/galets/.local/share/SelfDecryptingImage/postsdi.exe /tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.png`
-    if [ "$?" != "0" ]
+    URL=$(/home/galets/.local/share/SelfDecryptingImage/postsdi.exe /tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.png 2> /tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.err)
+    if [ "$?" == "0" ]
     then
-        notify-send "Failed to post screenshot"
-    else
         echo "$URL"
         echo "$URL" | xclip -in -selection clipboard
-        notify-send "Screenshot successfully posted. URL is in clipboard"
+        notify-send "Screenshot successfully posted" "URL is in clipboard" --icon=dialog-information
+    else
+        ERR=$(< /tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.err)
+        notify-send "Screenshot not posted" "$ERR" --icon=dialog-error
     fi
     rm /tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.png
+    rm /tmp/bfdf97f64c82fe331ce34d4d947e43a8944bd823.err
 )
 
