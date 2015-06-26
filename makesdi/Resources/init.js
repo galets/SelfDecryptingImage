1dae38f90b9ceeafe5f690267254afb985266012
galets <git5511@mx1.galets.net>
2015-06-25 18:02:48 -0700

    make image prompt for key if none supplied

--- makesdi/Resources/init.js
+++ makesdi/Resources/init.js
@@ -1,20 +1,46 @@
-﻿window.onload = function() {
-    setTimeout(function() {
-        try {
-            var key = window.location.hash.substring(1);
+﻿function show(control) {
+    control.style.display = 'block';
+}
 
-            var dataBytes = base64js.toByteArray(data);
-            var keyBytes = base64js.toByteArray(key);
-            var ivBytes = base64js.toByteArray(iv);
+function hide(control) {
+    control.style.display = 'none';
+}
 
-            var decryptedBytes = slowAES.decrypt(dataBytes, slowAES.modeOfOperation.CBC, keyBytes, ivBytes);
-            var decryptesBase64 = base64js.fromByteArray(decryptedBytes);
-            
-            text.style.display = 'none';
-            image.src="data:image/" + imageType + ";base64," + decryptesBase64;
-        }
-        catch(e) {
-            text.innerText = "This image cannot be decrypted. Please make sure you have the correct URL";
-        }
-    }, 100);
+function setUrlText(keyText) {
+    var href = window.location.href + "#" + keyText;
+    url1.href = href;
+    url1.innerText = href;
+}
+
+function decrypt(keyText) {
+    try {
+        show(text);
+
+        var dataBytes = base64js.toByteArray(data);
+        var keyBytes = base64js.toByteArray(keyText);
+        var ivBytes = base64js.toByteArray(iv);
+
+        var decryptedBytes = slowAES.decrypt(dataBytes, slowAES.modeOfOperation.CBC, keyBytes, ivBytes);
+        var decryptesBase64 = base64js.fromByteArray(decryptedBytes);
+        
+        hide(text);
+        hide(keyprompt);
+        image.src="data:image/" + imageType + ";base64," + decryptesBase64;
+        show(image);
+    }
+    catch(e) {
+        text.innerText = "This image cannot be decrypted. Please make sure you have the correct URL";
+    }
+}
+
+if (!window.location.hash || window.location.hash == "" || window.location.hash == "#") {
+    show(keyprompt);
+} else {
+    var key = window.location.hash.substring(1);
+    show(text);
+    window.onload = function() {
+        setTimeout(function() {
+            decrypt(key);
+        }, 100);
+    }
 }
